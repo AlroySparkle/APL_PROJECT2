587bfd405f574b49787eee129863795e01f1f1f0
Khalid Ahmad <ksa201207@gmail.com>
2021-04-17 19:32:42 +0300

    Firestore Added Completely

--- app/src/main/java/com/example/apl_project/MainActivity.java
+++ app/src/main/java/com/example/apl_project/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.apl_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button login;
     EditText SID, pass;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private Source source = Source.CACHE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,20 +36,106 @@ public class MainActivity extends AppCompatActivity {
         login = findViewById(R.id.loginBTN);
         SID = findViewById(R.id.SID);
         pass = findViewById(R.id.pass);
+
+
+
+
         login.setOnClickListener(v -> {
             String user = SID.getText().toString();
             String password = pass.getText().toString();
-            if(user.equals("321") && password.equals("123")) {
-                Intent i = new Intent(MainActivity.this, StudentPage.class);
-                Bundle extras = new Bundle();
-                extras.putString("ID", user);
-                extras.putString("NAME", "Ali");
-                i.putExtras(extras);
-                startActivity(i);
-            }
-            else{
-                Toast.makeText(getApplicationContext(),"username or password is wrong", Toast.LENGTH_SHORT).show();
-            }
+//            Here is the validation of the student
+
+
+
+            DocumentReference docRef = db.collection("Student").document(user);
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                            if(password.equals(document.getData().get("password"))){
+                                Toast.makeText(getApplicationContext(),"OK", Toast.LENGTH_LONG).show();
+                                Intent i = new Intent(MainActivity.this, StudentPage.class);
+                                Bundle extras = new Bundle();
+                                extras.putString("id", user);
+                                extras.putString("firstName", document.getData().get("firstName").toString());
+                                extras.putString("lastName", document.getData().get("lastName").toString());
+                                extras.putString("password", document.getData().get("password").toString());
+                                extras.putString("emailAddress", document.getData().get("emailAddress").toString());
+                                i.putExtras(extras);
+                                startActivity(i);
+                            }else{
+                                Toast.makeText(getApplicationContext(),"Password is wrong", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Log.d("TAG", "No such document");
+                            Toast.makeText(getApplicationContext(),"Username is wrong", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Log.d("TAG", "get failed with ", task.getException());
+                    }
+                }
+            });
+
+
+
+
+
+
+//            db.collection("Lecture").document("AkVEejWMRAFWubdaAXPx")
+//                    .get()
+//                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                            Log.d(String.valueOf(getApplicationContext()), "AAAQWWW: " + task.isSuccessful());
+//
+////                            if (task.isSuccessful()) {
+////                                Log.d("djjdjd" , "the lenght is " + task.getResult().size());
+////                                for (QueryDocumentSnapshot document : task.getResult()) {
+////                                    Log.d(String.valueOf(getApplicationContext()), "AAAQ: ");
+////                                    Log.d(String.valueOf(getApplicationContext()), "AAA: "+document.getId() + " => " + document.getData());
+////                                }
+////                            } else {
+////                                Log.w(String.valueOf(getApplicationContext()), "Error getting documents.", task.getException());
+////                            }
+//                        }
+//                    });
+
+
+
+//            db.collection("Student").whereEqualTo("emailAddress", "2160001234@iau.edu.sa")
+//                    .get()
+//                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+//                        @Override
+//                        public void onComplete(@NonNull Task<QuerySnapshot> task){
+//                            if (task.isSuccessful()){
+//                                for(QueryDocumentSnapshot document : task.getResult()){
+//                                    Toast.makeText(getApplicationContext(),"Datta: " + document.getData(), Toast.LENGTH_LONG).show();
+//                                    Log.d(String.valueOf(getApplicationContext()), document.getId() + " = >" + document.getData());
+//                                }
+//                            } else{
+//                                Toast.makeText(getApplicationContext(),"Error!!!", Toast.LENGTH_LONG).show();
+//                                Log.d(String.valueOf(getApplicationContext()), "Errorrrr!!!");
+//                            }
+//                        }
+//
+//                    });
+
+
+
+//            if(user.equals("321") && password.equals("123")) {
+//                Intent i = new Intent(MainActivity.this, StudentPage.class);
+//                Bundle extras = new Bundle();
+//                extras.putString("ID", user);
+//                extras.putString("NAME", "Ali");
+//                i.putExtras(extras);
+//                startActivity(i);
+//            }
+//            else{
+//                Toast.makeText(getApplicationContext(),"username or password is wrong", Toast.LENGTH_SHORT).show();
+//            }
         });
     }
 }
--- app/src/main/java/com/example/apl_project/StudentPage.java
+++ app/src/main/java/com/example/apl_project/StudentPage.java
@@ -1,9 +1,22 @@
 package com.example.apl_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,8 +28,9 @@ public class StudentPage extends AppCompatActivity {
     Button update;
     RecyclerView rl;
     int counter = 0;
-    private String name,id;
+    private String firstName,id, lastName, password, emailAddress, name;
     private String course[];
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -28,9 +42,43 @@ public class StudentPage extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if(extras != null)
         {
-            id = extras.getString("ID");
-            name = extras.getString("NAME");
+            id = extras.getString("id");
+            firstName = extras.getString("firstName");
+            lastName = extras.getString("lastName");
+            name = firstName + lastName;
+            emailAddress = extras.getString("emailAddress");
+            password = extras.getString("password");
         }
+
+
+
+
+        db.collection("Lecture")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task){
+                            if (task.isSuccessful()){
+                                for(QueryDocumentSnapshot document : task.getResult()){
+                                    Log.d("@Lecturs@ ", "------------------------------------------");
+                                    Log.d("@Lecturs@ ", "Subject_subjectId: " +document.getData().get("Subject_subjectId"));
+                                    Log.d("@Lecturs@ ", "State: " +document.getData().get("State"));
+                                    Log.d("@Lecturs@ ", "Teacher_teacherId: " +document.getData().get("Teacher_teacherId"));
+                                    Log.d("@Lecturs@ ", "time: " +document.getTimestamp("time").toDate());
+
+                                }
+                            } else{
+                                Toast.makeText(getApplicationContext(),"Error!!!", Toast.LENGTH_LONG).show();
+                                Log.d(String.valueOf(getApplicationContext()), "Errorrrr!!!");
+                            }
+                        }
+
+                    });
+
+
+
+
+
         SID.setText(id);
         SName.setText(name);
         update = findViewById(R.id.update);
